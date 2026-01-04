45f60b7770612725e11a0899981550a95be23791
ricemaster1 <153824981+ricemaster1@users.noreply.github.com>
2026-01-04 16:55:04 +1100

    chore: remove truecolor dir from tracking (now in .gitignore)

--- algorithms/truecolor/__init__.py
+++
@@ -1,4 +0,0 @@
-"""True Color converter - uses full 24-bit RGB hex values, no palette quantization."""
-from .src.truecolor import apply_truecolor, generate_assembly, process_image
-
-__all__ = ['apply_truecolor', 'generate_assembly', 'process_image']
